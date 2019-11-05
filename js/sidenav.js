b4aac52ee36788a2259d76ca0ca9d5c7b6dc5255
cortarelva <45341961+cortarelva@users.noreply.github.com>
2019-11-05 00:02:14 +0000

    mobile menu added

---
+++ js/sidenav.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("sidenav").style.width = "100%";
+    document.getElementById("menu-button").style.display = 'none';
+    }
+
+function closeNav(){
+    document.getElementById("sidenav").style.width = '0';
+    document.getElementById("menu-button").style.display = 'block';
+}    
